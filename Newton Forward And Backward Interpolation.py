4e0c69faab63cc8f45bececb8efd1311c1541243
Shuvo Saha <133127679+shuvoWBD@users.noreply.github.com>
2026-01-21 13:01:28 +0600

    Update Newton Forward And Backward Interpolation.py

--- Newton Forward And Backward Interpolation.py
+++ Newton Forward And Backward Interpolation.py
@@ -1,57 +1,80 @@
-# Python3 Program to interpolate using 
-# newton forward interpolation
+# Python3 Program for Newton Forward & Backward Interpolation
 
-# calculating u mentioned in the formula
-def u_cal(u, n):
-
-    temp = u;
+# Function to calculate u for forward interpolation
+def u_cal_forward(u, n):
+    temp = u
     for i in range(1, n):
-        temp = temp * (u - i);
-    return temp;
+        temp *= (u - i)
+    return temp
 
-# calculating factorial of given number n
+# Function to calculate u for backward interpolation
+def u_cal_backward(u, n):
+    temp = u
+    for i in range(1, n):
+        temp *= (u + i)
+    return temp
+
+# Factorial function
 def fact(n):
-    f = 1;
-    for i in range(2, n + 1):
-        f *= i;
-    return f;
+    f = 1
+    for i in range(2, n+1):
+        f *= i
+    return f
 
-# Driver Code
+# Newton Forward Interpolation
+def newton_forward(x, y_values, value):
+    n = len(x)
+    # Create forward difference table
+    y = [[0 for _ in range(n)] for __ in range(n)]
+    for i in range(n):
+        y[i][0] = y_values[i]
 
-# Number of values given
-n = 4;
-x = [ 45, 50, 55, 60 ];
-    
-# y[][] is used for difference table
-# with y[][0] used for input
-y = [[0 for i in range(n)]
-        for j in range(n)];
-y[0][0] = 0.7071;
-y[1][0] = 0.7660;
-y[2][0] = 0.8192;
-y[3][0] = 0.8660;
+    for i in range(1, n):
+        for j in range(n - i):
+            y[j][i] = y[j+1][i-1] - y[j][i-1]
 
-# Calculating the forward difference
-# table
-for i in range(1, n):
-    for j in range(n - i):
-        y[j][i] = y[j + 1][i - 1] - y[j][i - 1];
+    u = (value - x[0]) / (x[1] - x[0])
+    sum_val = y[0][0]
 
-# Displaying the forward difference table
-for i in range(n):
-    print(x[i], end = "\t");
-    for j in range(n - i):
-        print(y[i][j], end = "\t");
-    print("");
+    for i in range(1, n):
+        sum_val += (u_cal_forward(u, i) * y[0][i]) / fact(i)
 
-# Value to interpolate at
-value = 52;
+    return round(sum_val, 6)
 
-# initializing u and sum
-sum = y[0][0];
-u = (value - x[0]) / (x[1] - x[0]);
-for i in range(1,n):
-    sum = sum + (u_cal(u, i) * y[0][i]) / fact(i);
+# Newton Backward Interpolation
+def newton_backward(x, y_values, value):
+    n = len(x)
+    # Create backward difference table
+    y = [[0 for _ in range(n)] for __ in range(n)]
+    for i in range(n):
+        y[i][0] = y_values[i]
 
-print("\nValue at", value, 
-      "is", round(sum, 6));
+    for i in range(1, n):
+        for j in range(n-1, i-1, -1):
+            y[j][i] = y[j][i-1] - y[j-1][i-1]
+
+    u = (value - x[-1]) / (x[1] - x[0])
+    sum_val = y[n-1][0]
+
+    for i in range(1, n):
+        sum_val += (u_cal_backward(u, i) * y[n-1][i]) / fact(i)
+
+    return round(sum_val, 6)
+
+# -------------------- Example Usage --------------------
+
+# Forward Example
+x_forward = [45, 50, 55, 60]
+y_forward = [0.7071, 0.7660, 0.8192, 0.8660]
+value_forward = 52
+
+result_forward = newton_forward(x_forward, y_forward, value_forward)
+print("Newton Forward Interpolation: Value at", value_forward, "is", result_forward)
+
+# Backward Example
+x_backward = [1891, 1901, 1911, 1921, 1931]
+y_backward = [46, 66, 81, 93, 101]
+value_backward = 1925
+
+result_backward = newton_backward(x_backward, y_backward, value_backward)
+print("Newton Backward Interpolation: Value at", value_backward, "is", result_backward)
